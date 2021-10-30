dc66d618abb32571a8c488f550ee16a5aaf5119a
happyworld0905 <happyworld0905@gmail.com>
2021-10-30 10:47:13 +0300

    update

---
+++ src/constants/contractAddresses.js
@@ -0,0 +1,3 @@
+
+export const contractAddress = '0xA8d574a4da3A421F22F211043d46dc9A8531944e'; //vault contract address
+export const tokenAddress = '0xEEa0F6B10557BFb848C936571e6a93968Ba9EC34'; //repusd token address
